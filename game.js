4847ad16a861e030d4d2350e172d68990ec40f55
lingling86 <2627039229@qq.com>
2021-08-17 17:16:41 +0800

    upgrade

--- game.js
+++ game.js
@@ -1,21 +1,18 @@
-//伪常量
-var BLOCK_SIZE = 20; //格子大小
-var COLS = 40; //列数
-var ROWS = 30; //行数
-//变量
-var snakes = []; //保存蛇坐标
-var c = null; //绘图对象
-var toGo = 3; //行进方向
-var snakecount = 4; //蛇身数量
-var interval = null; //计时器
-var foodX = 0; //食物X轴坐标
-var foodY = 0; //食物Y轴坐标
-var oMark = null; //分数显示框
-var isPause = false; //是否暂停
-// 绘图函数
+var BLOCK_SIZE = 20;
+var COLS = 40;
+var ROWS = 30;
+var snakes = [];
+var c = null;
+var toGo = 3;
+var snakecount = 4;
+var interval = null;
+var foodX = 0;
+var foodY = 0;
+var oMark = null;
+var speed = 150;
+
 function draw() {
     c.clearRect(0, 0, BLOCK_SIZE * COLS, BLOCK_SIZE * ROWS);
-    //画出横线
     for (var i = 1; i <= ROWS; i++) {
         c.beginPath();
         c.moveTo(0, i * BLOCK_SIZE);
@@ -23,15 +20,13 @@ function draw() {
         c.strokeStyle = "gray";
         c.stroke();
     }
-    //画出竖线
     for (var i = 1; i <= COLS; i++) {
         c.beginPath();
         c.moveTo(i * BLOCK_SIZE, 0);
         c.lineTo(i * BLOCK_SIZE, BLOCK_SIZE * ROWS);
         c.stroke();
     }
-    //画出蛇
-    for (var i = 0; i < snakes.length; i++) {
+    for (var i = 0; i < snakes.length - 1; i++) {
         c.beginPath();
         c.fillStyle = "green";
         c.fillRect(snakes[i].x, snakes[i].y, BLOCK_SIZE, BLOCK_SIZE);
@@ -43,7 +38,17 @@ function draw() {
         c.strokeStyle = "white";
         c.stroke();
     }
-    //画出食物
+    c.beginPath();
+    c.fillStyle = "blue";
+    c.fillRect(snakes[snakes.length - 1].x, snakes[snakes.length - 1].y, BLOCK_SIZE, BLOCK_SIZE);
+    c.moveTo(snakes[snakes.length - 1].x, snakes[snakes.length - 1].y);
+    c.lineTo(snakes[snakes.length - 1].x + BLOCK_SIZE, snakes[snakes.length - 1].y);
+    c.lineTo(snakes[snakes.length - 1].x + BLOCK_SIZE, snakes[snakes.length - 1].y + BLOCK_SIZE);
+    c.lineTo(snakes[snakes.length - 1].x, snakes[snakes.length - 1].y + BLOCK_SIZE);
+    c.closePath();
+    c.strokeStyle = "white";
+    c.stroke();
+
     c.beginPath();
     c.fillStyle = "yellow";
     c.fillRect(foodX, foodY, BLOCK_SIZE, BLOCK_SIZE);
@@ -55,7 +60,7 @@ function draw() {
     c.strokeStyle = "red";
     c.stroke();
 }
-//游戏初始化
+
 function start() {
     for (var i = 0; i < snakecount; i++) {
         snakes[i] = {
@@ -67,28 +72,28 @@ function start() {
     draw();
     oMark.innerHTML = 0;
 }
-//移动函数
+
 function move() {
     switch (toGo) {
-        case 1: //左边
+        case 1:
             snakes.push({
                 x: snakes[snakecount - 1].x - BLOCK_SIZE,
                 y: snakes[snakecount - 1].y
             });
             break;
-        case 2: //上边
+        case 2:
             snakes.push({
                 x: snakes[snakecount - 1].x,
                 y: snakes[snakecount - 1].y - BLOCK_SIZE
             });
             break;
-        case 3: //右边
+        case 3:
             snakes.push({
                 x: snakes[snakecount - 1].x + BLOCK_SIZE,
                 y: snakes[snakecount - 1].y
             });
             break;
-        case 4: //下边
+        case 4:
             snakes.push({
                 x: snakes[snakecount - 1].x,
                 y: snakes[snakecount - 1].y + BLOCK_SIZE
@@ -102,7 +107,7 @@ function move() {
     isDie();
     draw();
 }
-//吃到食物判断
+
 function isEat() {
     if (snakes[snakecount - 1].x == foodX && snakes[snakecount - 1].y == foodY) {
         oMark.innerHTML = (parseInt(oMark.innerHTML) + 1).toString();
@@ -110,69 +115,107 @@ function isEat() {
         addSnake();
     }
 }
-//添加蛇身
+
 function addSnake() {
     snakecount++;
+    if (speed >= 500) {
+        speed -= 20;
+    } else {
+        speed = speed * 0.3;
+    }
     snakes.unshift({
         x: BLOCK_SIZE * COLS,
         y: BLOCK_SIZE * ROWS
     });
 }
-//交互响应函数
+
 function keydown(keyCode) {
     switch (keyCode) {
-        case 37: //左边
+        case 37:
             if (toGo != 1 && toGo != 3) toGo = 1;
             break;
-        case 38: //上边
+        case 38:
             if (toGo != 2 && toGo != 4) toGo = 2;
             break;
-        case 39: //右边
+        case 39:
             if (toGo != 3 && toGo != 1) toGo = 3;
             break;
-        case 40: //下的
+        case 40:
             if (toGo != 4 && toGo != 2) toGo = 4;
-            break;
-        case 80: //开始/暂停
-            if (isPause) {
-                interval = setInterval(move, 100);
-                isPause = false;
-                document.getElementById('pause').innerHTML = "Pause";
-            } else {
-                clearInterval(interval);
-                isPause = true;
-                document.getElementById('pause').innerHTML = "Start";
-            }
             break;
     }
 }
-//制造食物
+
 function addFood() {
-    foodX = Math.floor(Math.random() * (COLS - 1)) * BLOCK_SIZE;
-    foodY = Math.floor(Math.random() * (ROWS - 1)) * BLOCK_SIZE;
-    // console.log(foodX + " -- " + foodY);
+    var flag;
+    var fx, fy;
+    while (1) {
+        flag = 1;
+        fx = Math.floor(Math.random() * (COLS - 1)) * BLOCK_SIZE;
+        fy = Math.floor(Math.random() * (ROWS - 1)) * BLOCK_SIZE;
+        for (var i = 0; i < snakecount; i++) {
+            if (fx == snakes[i].x && fy == snakes[i].y) {
+                flag = 0;
+                break;
+            }
+        }
+        if (flag) break;
+    }
+    foodX = fx;
+    foodY = fy;
 }
-//死亡判断
+
 function isDie() {
     if (snakes[snakecount - 1].x == -20 || snakes[snakecount - 1].x == BLOCK_SIZE * COLS || snakes[snakecount - 1].y == -20 || snakes[snakecount - 1].y == BLOCK_SIZE * ROWS) {
         alert("Game Over!");
         clearInterval(interval);
+        change();
+        localStorage.setItem('e',parseInt(oMark.innerHTML));
     }
     for (var i = 0; i < snakecount - 1; i++) {
         if (snakes[snakecount - 1].x == snakes[i].x && snakes[snakecount - 1].y == snakes[i].y) {
-            clearInterval(interval);
             alert("Game Over!");
+            clearInterval(interval);
+            change();
+            localStorage.setItem('e',parseInt(oMark.innerHTML));
         }
     }
 }
-// 启动函数
-window.onload = function() {
+
+function change(){  
+    localStorage.setItem('a',localStorage.getItem('b'));
+    localStorage.setItem('b',localStorage.getItem('c'));
+    localStorage.setItem('c',localStorage.getItem('d'));
+    localStorage.setItem('d',localStorage.getItem('e'));
+}
+
+window.onload = function () {
     c = document.getElementById('canvas').getContext('2d');
     oMark = document.getElementById('mark_con');
     start();
-    interval = setInterval(move, 100);
-    document.onkeydown = function(event) {
+    interval = setInterval(move, speed);
+    document.onkeydown = function (event) {
         var event = event || window.event;
         keydown(event.keyCode);
     }
+    var myChart = echarts.init(document.getElementById('main'));
+                    var option = {
+                        title: {
+                            text: '记录'
+                        },
+                        tooltip: {},
+                        legend: {
+                            data:['分数']
+                        },
+                        xAxis: {
+                            data: ["1","2","3","4","5"]
+                        },
+                        yAxis: {},
+                        series: [{
+                            name: '分数',
+                            type: 'bar',
+                            data: [localStorage.getItem('a'), localStorage.getItem('b'), localStorage.getItem('c'),localStorage.getItem('d'), localStorage.getItem('e')]
+                        }]
+                    };
+                    myChart.setOption(option);
 }
